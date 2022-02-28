631e451ec83956c411ae0cd3a4cc71a76a2a7df8
Anh Phuong Do <71036845+anhphuongdo34@users.noreply.github.com>
2022-02-28 01:03:45 -0500

    normalize image data, clean up all the unnecessary comments

--- CNN_Affect/Training_Procedure.py
+++ CNN_Affect/Training_Procedure.py
@@ -27,17 +27,6 @@ class Training_Procedure:
 
         self.regression = regression
 
-    # def load_image(path, num_arr) :
-    #     images = []
-    #     for num in num_arr :
-    #         images.append(np.asarray(Image(path + "/" + num + ".jpg")))
-
-    #     return np.asarray(images)
-
-    # function to normalize the image data from [0, 255] to [0, 1]
-    # def data_normalize(self, image):
-    #     return image.as_numpy() / 255.0
-
     # have yet known how to load data for regression
     #   since the label values must be type integers
 
@@ -54,7 +43,7 @@ class Training_Procedure:
             image_size=(self.image_size, self.image_size),
             batch_size=self.batch_size
         )
-        return train_data
+        return train_data.apply(lambda x, y: (x/255., y))
 
     def load_test_data(self):
         test_data = tf.keras.preprocessing.image_dataset_from_directory(
@@ -66,43 +55,13 @@ class Training_Procedure:
             image_size=(self.image_size, self.image_size),
             batch_size=self.batch_size
         )
-        return test_data
+        return test_data.apply(lambda x, y: (x/255., y))
 
     # Training function
-    # def training(self, train_data, val_data) :
-    #     # Training process
-    #     startTime = timeit.default_timer()
-    #     hist = self.model.fit(train_data,
-    #                           validation_data=val_data,
-    #                           epochs=self.epochs,
-    #                           batch_size=self.batch_size, verbose=1)
-    #     print('Training time:', timeit.default_timer() - startTime)
-        
-    #     return hist
-
-    # Testing function
-    # def testing(self, test_data) :
-    #     # Testing
-    #     startTime = timeit.default_timer()
-    #     testLoss, testAccuracy = self.model.evaluate(test_data)
-    #     print('Testing time:', timeit.default_timer() - startTime)
-
-    #     return testLoss, testAccuracy
-
-    def process_classification(self):
-
+    def training(self):
         train_data = self.load_train_data()
-
-        # print(train_data.element_spec)
-        # for i in train_data:
-        #     print(i)
-        #     break
-
         val_data = self.load_train_data(sub_set="validation")
-        test_data = self.load_test_data()
-
-        # Training
-        # hist = self.training(train_data=train_data, val_data=val_data)
+        # Training process
         startTime = timeit.default_timer()
         hist = self.model.fit(train_data,
                               validation_data=val_data,
@@ -110,16 +69,32 @@ class Training_Procedure:
                               batch_size=self.batch_size, verbose=1)
         print('Training time:', timeit.default_timer() - startTime)
 
+        return hist
 
+    # Testing function
+    def testing(self):
         # Testing
-        # testLoss, testAccuracy = self.testing(test_data=test_data)
+        test_data = self.load_test_data()
         startTime = timeit.default_timer()
         testLoss, testAccuracy = self.model.evaluate(test_data)
         print('Testing time:', timeit.default_timer() - startTime)
 
+        return testLoss, testAccuracy
+
+    def process_classification(self):
+
+        # Training
+        hist = self.training()
+
+        # Testing
+        testLoss, testAccuracy = self.testing()
 
         return hist.history, testLoss, testAccuracy, self.model.count_params()
+
+    def process_regression(self):
+        pass
 
     def process(self):
         if (not self.regression):
             return self.process_classification()
+        return self.process_regression()

8ba6ba34c335ad21fa198fa58285af1416c1c849
hieumtran <hieutran_2023@depauw.edu>
2022-03-09 20:28:13 -0500

    from server

--- PyTorch_reg/trainprocedure.py
+++ PyTorch_reg/trainprocedure.py
@@ -19,14 +19,16 @@ def eval(input, output,
          model, training):
     input = torch.reshape(input, (-1, 3, 128, 128))
     predict = model(input.float())
-    loss = torch.sqrt(loss_func(predict.float(), output.float()))
+    val_loss = loss_func(predict[:, 0].float(), output[:, 0].float())
+    ars_loss = loss_func(predict[:, 1].float(), output[:, 1].float())
+    loss = loss_func(predict.float(), output.float())
     # print(loss)
     if training: 
         optimizer.zero_grad()
         loss.requres_grad = True
         loss.backward()
         optimizer.step()
-    return loss.item()
+    return loss.item(), val_loss.item(), ars_loss.item()
 
 def fit(train_data, val_data, 
         model,
@@ -38,50 +40,79 @@ def fit(train_data, val_data,
     x_val, y_val = val_data
 
     # Initialize loss
-    train_loss = []
-    val_loss = []
+    train_loss_total = []
+    train_loss_val = []
+    train_loss_ars = []
+    val_loss_total = []
+    val_loss_val = []
+    val_loss_ars = []
 
     for epoch in range(1, epochs+1):
         model.train()
-        samples = 0
-        train_avg_loss = 0
-        val_avg_loss = 0
 
-        # Training 
+        train_samples = 0
+        val_samples = 0
+        batch_train = 0
+        batch_val = 0
+
+        avg_train_total_loss = 0
+        avg_train_val_loss = 0
+        avg_train_ars_loss = 0
+        avg_val_total_loss = 0
+        avg_val_val_loss = 0
+        avg_val_ars_loss = 0
+        
+        train_templ = 'Training {}: total: {} / val: {} / ars: {} / Time: {}s'
+        val_templ = 'Validation {}: total: {} / val: {} / ars: {} / Time: {}s'
+        # Training
         for (batchX, batchY) in batch(x_train, y_train, batch_size):
             start = timeit.default_timer()
             (batchX, batchY) = (torch.from_numpy(batchX).to(device), torch.from_numpy(batchY).to(device))
-            loss = eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=True)
-            train_avg_loss += loss
-            samples += 1
-            print('Mini-batch {}: {} - {}s'.format(samples, loss, timeit.default_timer()-start))
-        train_avg_loss = train_avg_loss / samples # Average loss across mini-batch
-        train_loss.append(train_avg_loss)
+            loss, val_loss, ars_loss = eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=True)
+            avg_train_total_loss += loss
+            avg_train_val_loss += val_loss
+            avg_train_ars_loss += ars_loss
+            train_samples += batchY.size(0)
+            batch_train += 1
+            print(train_templ.format(batch_train, np.sqrt(loss/batchY.size(0)), np.sqrt(val_loss/batchY.size(0)), np.sqrt(ars_loss/batchY.size(0)), timeit.default_timer()-start))
+        train_loss_total.append(np.sqrt(avg_train_total_loss / train_samples))
+        train_loss_val.append(np.sqrt(avg_train_val_loss / train_samples))
+        train_loss_ars.append(np.sqrt(avg_train_ars_loss / train_samples))
 
         
         # Evaluation
-        samples = 0
         model.eval() # Canceling regularization
         for (batchX, batchY) in batch(x_val, y_val, batch_size):
+            start = timeit.default_timer()
             (batchX, batchY) = (torch.from_numpy(batchX).to(device), torch.from_numpy(batchY).to(device))
-            val_avg_loss += eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=False)
-            samples += 1
-        val_avg_loss = val_avg_loss / samples # Average loss across mini-batch
-        val_loss.append(val_avg_loss)
+            loss, val_loss, ars_loss = eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=False)
+            avg_val_total_loss += loss
+            avg_val_val_loss += val_loss
+            avg_val_ars_loss += ars_loss
+            val_samples += batchY.size(0)
+            batch_val += 1
+            print(val_templ.format(batch_val, np.sqrt(loss/batchY.size(0)), np.sqrt(val_loss/batchY.size(0)), np.sqrt(ars_loss/batchY.size(0)), timeit.default_timer()-start))
+        val_loss_total.append(np.sqrt(avg_val_total_loss / val_samples))
+        val_loss_val.append(np.sqrt(avg_val_val_loss / val_samples))
+        val_loss_ars.append(np.sqrt(avg_val_ars_loss / val_samples))
         
         torch.save({
                     'epoch': epoch,
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
-                    'train_loss': train_loss,
-                    'val_loss': val_loss
+                    'train_loss_total': train_loss_total,
+                    'train_loss_val': train_loss_val,
+                    'train_loss_ars': train_loss_ars,
+                    'val_loss': val_loss,
+                    'val_loss_val': val_loss_val,
+                    'val_loss_ars': val_loss_ars
                     }, save_path+str(epoch)+'.pt')
 
         # display model progress on the current training batch
-        template = "epoch: {} train loss: {} val loss: {}"
-        print(template.format(epoch, train_avg_loss, val_avg_loss))
+        template = 'Epoch {}: train_total: {} / train_al: {} / train_ars: {} / val_total: {} / val_al: {} / val_ars: {}'
+        print(template.format(epoch, np.sqrt(avg_train_total_loss / train_samples), np.sqrt(avg_train_val_loss / train_samples), np.sqrt(avg_train_ars_loss / train_samples), np.sqrt(avg_val_total_loss / val_samples), np.sqrt(avg_val_val_loss / val_samples), np.sqrt(avg_val_ars_loss / val_samples)))
 
-def test_model(test_data, 
+def test_model(test_data, aspect, 
                 model,
                 optimizer, loss_func, 
                 batch_size,
@@ -89,23 +120,30 @@ def test_model(test_data,
     x_test, y_test = test_data
     
     ckpt = torch.load(load_path)
-    plt.plot(ckpt['train_loss'])
-    plt.plot(ckpt['val_loss'])
+    plt.plot(ckpt[aspect[0]])
+    plt.plot(ckpt[aspect[1]])
     plt.grid()
-    plt.show()
+    plt.savefig('./sp_trans.jpg')
+    # plt.show()
 
     model.load_state_dict(ckpt['model_state_dict'])
     optimizer.load_state_dict(ckpt['optimizer_state_dict'])
 
-    test_avg_loss = 0
-    samples = 0
+    avg_test_total_loss = 0
+    avg_test_val_loss = 0
+    avg_test_ars_loss = 0
+    test_samples = 0
     model.eval() # Canceling regularization
     for (batchX, batchY) in batch(x_test, y_test, batch_size):
         (batchX, batchY) = (torch.from_numpy(batchX).to(device), torch.from_numpy(batchY).to(device))
-        test_avg_loss += eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=False)
-        samples += 1
-    test_avg_loss = test_avg_loss / samples # Average loss across mini-batch
-    print('Test loss: ', test_avg_loss)
+        loss, val_loss, ars_loss = eval(input=batchX, output=batchY, optimizer=optimizer, loss_func=loss_func, model=model, training=False)
+        avg_test_total_loss += loss
+        avg_test_val_loss += val_loss
+        avg_test_ars_loss += ars_loss
+        print(batchY.size())
+        test_samples += batchY.size(0)
+    template = 'Test loss: {} / val_loss: {} / ars_loss: {} '
+    print(template.format(np.sqrt(avg_test_total_loss/ test_samples), np.sqrt(avg_test_val_loss/ test_samples), np.sqrt(avg_test_ars_loss/ test_samples))) 
 
 
     
--- PyTorch_reg/vgg_simple.py
+++ PyTorch_reg/vgg_simple.py
@@ -25,8 +25,8 @@ class VGG_simple(nn.Module):
 
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(32768, 256)
-        self.fc2 = nn.Linear(256, 128)
+        self.fc1 = nn.Linear(8192, 4096)
+        self.fc2 = nn.Linear(4096, 128)
         self.fc3 = nn.Linear(128, 2)
 
     def forward(self, x):
@@ -34,25 +34,29 @@ class VGG_simple(nn.Module):
         x = F.relu(self.conv1_1(x))
         x = F.relu(self.conv1_2(x))
         x = self.maxpool(x)
+        x = F.dropout(x, 0.5)
         # Layer 2
         x = F.relu(self.conv2_1(x))
         x = F.relu(self.conv2_2(x))
         x = self.maxpool(x)
-        # # Layer 3
+        x = F.dropout(x, 0.5)
+        # Layer 3
         x = F.relu(self.conv3_1(x))
-        # x = F.relu(self.conv3_2(x))
-        # x = F.relu(self.conv3_3(x))
+        x = F.relu(self.conv3_2(x))
+        x = F.relu(self.conv3_3(x))
         x = self.maxpool(x)
-        # # Layer 4
+        # Layer 4
         x = F.relu(self.conv4_1(x))
-        # x = F.relu(self.conv4_2(x))
-        # x = F.relu(self.conv4_3(x))
+        x = F.relu(self.conv4_2(x))
+        x = F.relu(self.conv4_3(x))
         x = self.maxpool(x)
-        # # Layer 5
-        # x = F.relu(self.conv5_1(x))
-        # x = F.relu(self.conv5_2(x))
-        # x = F.relu(self.conv5_3(x))
-        # x = self.maxpool(x)
+        x = F.dropout(x, 0.5)
+        # Layer 5
+        x = F.relu(self.conv5_1(x))
+        x = F.relu(self.conv5_2(x))
+        x = F.relu(self.conv5_3(x))
+        x = self.maxpool(x)
+        x = F.dropout(x, 0.5)
 
         # Flatten
         x = x.reshape(x.shape[0], -1)

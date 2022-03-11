e1fa9915a9f3f28f2ece84322f91372cbe26b647
hieumtran <hieutran_2023@depauw.edu>
2022-03-11 02:10:45 -0500

    Add test procedure, adjust train_test_val function, deprecated visualization

--- PyTorch_reg/procedure.py
+++ PyTorch_reg/procedure.py
@@ -7,7 +7,8 @@ import datetime
 
 
 class procedure:
-    def __init__(self, optimizer, loss_func, model, start_epoch, end_epoch, device):
+    def __init__(self, optimizer, loss_func, model, start_epoch, end_epoch, / 
+                save_path, save_fig, device):
         self.optimizer = optimizer  # optimizer
         self.loss_func = loss_func # loss function
         self.model = model  # model init
@@ -16,20 +17,24 @@ class procedure:
         self.end_epoch = end_epoch # Ending epoch
         self.train_arr = [] # Init training loss
         self.val_arr = [] # Init validation loss
+        self.save_path = save_path # Checkpoint directory
+        self.save_fig = save_fig # Figure saving
 
     def fit(self, train_loader, val_loader):
-        # Epoch training
         output_template = 'Epoch {} / train: {:.10f} / val: {:.10f}  / Time: {:.5f}s / Current date & Time: {:%Y-%m-%d %H:%M:%S}'
         for epoch in range(self.start_epoch, self.end_epoch + 1):
             epoch_start = timeit.default_timer()
-            train_loss = self.train(train_loader, training=True)
-            val_loss = self.train(val_loader, training=False)
+            
+            # Update loss and optimizer
+            train_loss = self.train_val_test(train_loader, training=True)
+            val_loss = self.train_val_test(val_loader, training=False)
             self.train_arr.append(train_loss)
-            self.val_arr.append(val_loss)
+            self.val_arr.append(val_loss) 
+
+            self.save(epoch) # Saving model 
             print(output_template.format(epoch, train_loss, val_loss, timeit.default_timer() - epoch_start, datetime.datetime.now()))
             
-    # Training and Validation
-    def train(self, loader, training):
+    def train_val_test(self, loader, training):
         if training: 
             self.model.train()
             log_template = 'Training mini-batch {}: {:.10f} / Time: {:.5f}s / Current date & Time: {:%Y-%m-%d %H:%M:%S}'
@@ -40,7 +45,7 @@ class procedure:
         # Init computation variables
         samples, batch_cnt, sum_loss = 0, 0, 0
         for (batchX, batchY) in loader:
-            start = timeit.default_timer() # Init time
+            start = timeit.default_timer()
             (batchX, batchY) = (batchX.to(self.device), batchY.to(self.device)) # Load data
             if training: self.optimizer.zero_grad() # Zero grad before mini-batch
             loss = self.loss_compute(batchX, batchY) # Forward model
@@ -57,7 +62,6 @@ class procedure:
 
             # Logging
             print(log_template.format(batch_cnt, loss/(2*batchY.size(0)), timeit.default_timer()-start, datetime.datetime.now()))
-        
         return sum_loss/(2*samples)
     
     def loss_compute(self, input, output):
@@ -65,15 +69,21 @@ class procedure:
         predict = self.model(input.float())
         loss = self.loss_func(predict.float(), output.float(), output.size(0))
         return loss.item()*(2*output.size(0))
+    
+    def test(self, test_loader):
+        start = timeit.default_timer()
+        output_template = 'Test: {:.10f} / Time: {:.5f}s / Current date & Time: {:%Y-%m-%d %H:%M:%S}'
+        test_loss = self.train_val_test(test_loader, training=False)
+        print(output_template.format(test_loss, timeit.default_timer() - start, datetime.datetime.now()))
 
-    def save_model(self, curr_epoch, save_path):
+    def save_model(self, curr_epoch):
         torch.save({
             'epoch': curr_epoch,
             'model_state_dict': self.model.state_dict(),
             'optimizer_state_dict': self.optimizer.state_dict(),
             'train_arr': self.train_arr,
             'val_arr': self.val_arr
-        }, save_path + str(curr_epoch) + '.pt')
+        }, self.save_path + str(curr_epoch) + '.pt')
     
     def load_model(self, load_path):
         ckpt = torch.load(load_path)
@@ -82,3 +92,6 @@ class procedure:
         self.start_epoch = ckpt['epoch']
         self.train_arr = ckpt['train_arr']
         self.val_arr = ckpt['val_arr']
+
+    def visualize(self, save_fig):
+        pass

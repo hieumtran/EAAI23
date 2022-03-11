0964064ab40fd3ab3be511e9d51a4a9634ab80d7
hieumtran <hieutran_2023@depauw.edu>
2022-03-11 01:47:27 -0500

    Update new main.py

--- PyTorch_reg/main.py
+++ PyTorch_reg/main.py
@@ -9,24 +9,8 @@ from design.resnet import ResNet
 from loss_function import L2_dist
 
 
-def main():
-    # Regression model for
-    train_paths, train_labels = path_loader('train', 'reg')
-    val_paths, val_labels = path_loader('val', 'reg')
-    test_paths, test_labels = path_loader('test', 'reg')
-
-    # Parameters
-    batch_size = 64
-    epochs = 24
-    loss_func = L2_dist
-    save_path = './PyTorch_reg/design/resnet/resnet_'
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    # Init model & Optimizer
-    model = ResNet(256, [64, 256], 1).to(device).float()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    # Data Loading
+def load_data(root, batch_size, num_workers, shuffle)
+    # Data loading
     train_image_dir = "data/train_set/images/"
     train_reg_frame = "train_reg.csv"
 
@@ -36,6 +20,38 @@ def main():
     test_image_dir = "data/val_set/images/"
     test_reg_frame = "test_reg.csv"
 
+    train_loader = Dataloader(root=root, image_dir=train_image_dir, label_frame=train_reg_frame, regression=True).load_batch(
+                                batch_size=batch_size, shuffle=shuffle, num_workers=num_worker)
+    val_loader = Dataloader(root=root, image_dir=val_image_dir, label_frame=val_reg_frame, regression=True).load_batch(
+                                batch_size=batch_size, shuffle=shuffle, num_workers=num_worker)
+    test_loader = Dataloader(root=root, image_dir=test_image_dir, label_frame=test_reg_frame, regression=True).load_batch(
+                                batch_size=batch_size, shuffle=shuffle, num_workers=num_worker)
+    
+    return train_loader, val_loader, test_loader
+
+def main():
+    # Regression model for
+    train_paths, train_labels = path_loader('train', 'reg')
+    val_paths, val_labels = path_loader('val', 'reg')
+    test_paths, test_labels = path_loader('test', 'reg')
+
+    # Data parameters
+    batch_size = 64
+    num_workers = 2
+    root_dir = './'
+    shuffle = False
+    train_loader, val_loader, test_loader = load_data(root=root_dir, batch_size=batch_size
+                                                        num_workers=num_workers, shuffle=shuffle)
+    
+    # Model parameters
+    epochs = 24
+    loss_func = L2_dist
+    save_path = './PyTorch_reg/design/resnet/resnet_'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Init model & Optimizer
+    model = ResNet(256, [64, 256], 1).to(device).float()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 if __name__ == '__main__':
     main()

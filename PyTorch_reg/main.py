aa15d218f60c56793c1a072867a5806463582721
hieumtran <hieutran_2023@depauw.edu>
2022-03-11 14:05:23 -0500

    Adjust index

--- PyTorch_reg/main.py
+++ PyTorch_reg/main.py
@@ -20,14 +20,15 @@ def load_data(root, batch_size, subset, num_workers, shuffle):
     test_image_dir = "data/val_set/images/"
     test_reg_frame = "test_reg.csv"
 
-    train_loader = Dataloader(root=root, image_dir=train_image_dir, subset=subset, label_frame=train_reg_frame, regression=True).load_batch(
-                                batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    val_loader = Dataloader(root=root, image_dir=val_image_dir, subset=subset, label_frame=val_reg_frame, regression=True).load_batch(
-                                batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    test_loader = Dataloader(root=root, image_dir=test_image_dir, subset=subset, label_frame=test_reg_frame, regression=True).load_batch(
-                                batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    
-    return train_loader, val_loader, test_loader
+    train = Dataloader(root=root, image_dir=train_image_dir, subset=subset, label_frame=train_reg_frame,
+                       regression=True).load_batch(batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    val = Dataloader(root=root, image_dir=val_image_dir, subset=subset, label_frame=val_reg_frame,
+                     regression=True).load_batch(batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    test = Dataloader(root=root, image_dir=test_image_dir, subset=subset, label_frame=test_reg_frame,
+                      regression=True).load_batch(batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    return train, val, test
+
 
 def main():
     # Data parameters
@@ -36,15 +37,15 @@ def main():
     subset = None
     root_dir = './'
     shuffle = False
-    train_loader, val_loader, test_loader = load_data(root=root_dir, batch_size=batch_size, \
+    train_loader, val_loader, test_loader = load_data(root=root_dir, batch_size=batch_size,
                                                       subset=subset, num_workers=num_workers, shuffle=shuffle)
-    
+
     # Model parameters
     start_epoch = 1
     end_epoch = 24
     loss_func = L2_dist
     save_path = './PyTorch_reg/design/resnet/resnet_'
-    save_fig = './PyTroch_reg/figure/ResNet_loss'
+    save_fig = './PyTorch_reg/figure/ResNet_loss'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Init model & Optimizer
@@ -52,10 +53,11 @@ def main():
     optimizer = torch.optim.SGD(res_net.parameters(), lr=1e-3)
 
     # procedure init
-    proced = procedure(optimizer=optimizer, loss_func=loss_func, model=res_net, \
-                        start_epoch=start_epoch, end_epoch=end_epoch, device=device, \
+    proceed = procedure(optimizer=optimizer, loss_func=loss_func, model=res_net,
+                        start_epoch=start_epoch, end_epoch=end_epoch, device=device,
                         save_path=save_path, save_fig=save_fig)
-    proced.fit(train_loader, val_loader)
+    proceed.fit(train_loader, val_loader)
+
 
 if __name__ == '__main__':
     main()

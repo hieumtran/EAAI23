45d1ce1f17b2ae249ec04538e884589f7c74ac0c
hieumtran <hieutran_2023@depauw.edu>
2022-03-11 01:25:06 -0500

    Update loss function format. Update main_torch.py

--- PyTorch_reg/main_torch.py
+++ PyTorch_reg/main_torch.py
@@ -1,6 +1,6 @@
 import numpy as np
-from dataloader import DataLoader, path_loader # Uncertain on DataLoader - Anh
-from trainprocedure import fit, test_model # Wrong test_model function
+from dataloader import DataLoader, path_loader  # Uncertain on DataLoader - Anh
+from trainprocedure import fit, test_model  # Wrong test_model function
 import torch
 import torch.nn as nn
 # from design.vgg_simple import VGG_simple # VGG-16
@@ -8,41 +8,34 @@ import torch.nn as nn
 from design.resnet import ResNet
 from loss_function import L2_dist
 
+
 def main():
     # Regression model for
-    train_paths, train_labels = path_loader('train', 'reg') 
+    train_paths, train_labels = path_loader('train', 'reg')
     val_paths, val_labels = path_loader('val', 'reg')
     test_paths, test_labels = path_loader('test', 'reg')
 
-    # Hyperparameters
+    # Parameters
     batch_size = 64
     epochs = 24
     loss_func = L2_dist
-    save_path = './PyTorch_reg/design/resnet/resnet_'    
+    save_path = './PyTorch_reg/design/resnet/resnet_'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Init model & Optimizer
     model = ResNet(256, [64, 256], 1).to(device).float()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    
-    # fit(train_data=(train_paths, train_labels), 
-    #     val_data=(val_paths, val_labels), 
-    #     model=model,
-    #     optimizer=optimizer, loss_func=loss_func, 
-    #     epochs=epochs, batch_size=batch_size,
-    #     save_path=save_path, device=device)
+    # Data Loading
+    train_image_dir = "data/train_set/images/"
+    train_reg_frame = "train_reg.csv"
 
-    test_model(test_data=(test_paths, test_labels),  aspect = ['train_loss', 'val_loss'],
-                model=model,
-                optimizer=optimizer, loss_func=loss_func, 
-                batch_size=batch_size,
-                load_path=save_path + '24.pt', device=device)
-    
+    val_image_dir = "data/train_set/images/"
+    val_reg_frame = "val_reg.csv"
 
+    test_image_dir = "data/val_set/images/"
+    test_reg_frame = "test_reg.csv"
 
-    # for (batchX, batchY) in batch(train_paths[:5], train_labels[:5], 1):
-    #     plt.imshow(batchX.squeeze())
-    #     plt.title('Valence: {} / Arousal: {}'.format(batchY.squeeze()[0], batchY.squeeze()[1]))
-    #     plt.show()
 
 if __name__ == '__main__':
     main()

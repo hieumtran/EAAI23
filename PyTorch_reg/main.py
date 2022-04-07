934d4036ab067bb14e2a7ad1c288708758cd7f60
hieumtran <hieutran_2023@depauw.edu>
2022-04-06 21:00:26 -0400

    Update main function

--- PyTorch_reg/main.py
+++ PyTorch_reg/main.py
@@ -5,14 +5,15 @@ import torch
 import torch.nn as nn
 # from design.vgg_simple import VGG_simple # VGG-16
 # from design.sp_trans import sp_trans # Spatial Transformer 
-from design.resnet import ResNet
+# from design.resnet import ResNet
+from design.simplenet import simpleNet
 from loss_function import L2_dist
 
 
-def load_data(root, batch_size, num_workers, subset, shuffle):
+def load_data(root, batch_size, num_workers, subset, shuffle, validation):
     # Data loading
     train_image_dir = "data/train_set/images/"
-    train_reg_frame = "train_reg.csv"
+    train_reg_frame = "train_subset_reg.csv"
 
     val_image_dir = "data/train_set/images/"
     val_reg_frame = "val_reg.csv"
@@ -27,46 +28,48 @@ def load_data(root, batch_size, num_workers, subset, shuffle):
     test = Dataloader(root=root, image_dir=test_image_dir, label_frame=test_reg_frame, subset=subset,
                       regression=True).load_batch(batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
+    if validation == False: val = None
     return train, val, test
 
 
 def main():
     # Data parameters
-    batch_size = 16
+    batch_size = 8
     num_workers = 0
     subset = None
     # subset = 1000
     root_dir = './'
     shuffle = False
-    train_loader, val_loader, test_loader = load_data(root=root_dir, batch_size=batch_size, subset=subset, num_workers=num_workers, shuffle=shuffle)
+    train_loader, val_loader, test_loader = load_data(root=root_dir, batch_size=batch_size, subset=subset, 
+                                                      num_workers=num_workers, shuffle=shuffle, validation=False)
 
     # Model parameters
-    start_epoch = 51
-    end_epoch = 60
+    start_epoch = 1
+    end_epoch = 30
     loss_func = L2_dist
-    save_path = './PyTorch_reg/design/resnet/resnet50_'
-    save_fig = './PyTorch_reg/figure/ResNet_loss'
+    save_path = './PyTorch_reg/design/simplenet/simplenet_'
+    save_fig = './PyTorch_reg/figure/simplenet_loss'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Init model & Optimizer
     # res_net = ResNet(res_learning=[3, 4, 23, 3]).to(device).float()
-    res_net = ResNet(res_learning=[3, 4, 6, 3]).to(device).float()
-    optimizer = torch.optim.SGD(res_net.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
+    # res_net = ResNet(res_learning=[3, 4, 6, 3]).to(device).float()
+    # optimizer = torch.optim.SGD(res_net.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
+
+    simple_net = simpleNet(3, 2).to(device)
+    optimizer = torch.optim.SGD(simple_net.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.0001)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3)
 
     # procedure init
     proceed = procedure(optimizer=optimizer, scheduler=scheduler,
-                        loss_func=loss_func, model=res_net,
+                        loss_func=loss_func, model=simple_net,
                         start_epoch=start_epoch, end_epoch=end_epoch, device=device,
                         save_path=save_path, save_fig=save_fig)
-    proceed.load_model('./PyTorch_reg/design/resnet/resnet50_50.pt')
     proceed.fit(train_loader, val_loader)
     # for i in range(1, 51):
         # proceed.load_model('./PyTorch_reg/design/resnet/resnet50_' + str(i) + '.pt')
         # proceed.test(test_loader)
     # proceed.visualize('./PyTorch_reg/figure/ResNet50_loss.jpg')
         
-
-
 if __name__ == '__main__':
     main()

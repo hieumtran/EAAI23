c2ecec7e8fd160c76502a7ebe227823f51119256
hieumtran <hieutran_2023@depauw.edu>
2022-06-07 17:00:17 -0400

    Update dataloader for multitask model

--- PyTorch_reg/dataloader.py
+++ PyTorch_reg/dataloader.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 
 # inherit the torch.utils.data.Dataset class
 class Dataset(Dataset):
-    def __init__(self, image_dir, label_frame, regression=False, subset=None, transform=None):
+    def __init__(self, image_dir, label_frame, mode, subset=None, transform=None):
         """
         Args:
             image_dir (string): Directory with all the images.
@@ -19,7 +19,8 @@ class Dataset(Dataset):
         """
         self.image_dir = image_dir
         self.label_frame = pd.read_csv(label_frame)
-        self.regression = regression
+        assert mode in ['reg', 'class', 'both']
+        self.mode = mode
         self.transform = transform
         if subset != None: self.label_frame = self.label_frame[:subset]
         
@@ -33,30 +34,34 @@ class Dataset(Dataset):
         img_name = os.path.join(self.image_dir,
                                 self.label_frame.iloc[idx, 0])
         images = Image.open(img_name)
-        
-        if (self.regression):
-            labels = self.label_frame.iloc[idx, 1:]
-            labels = np.array([labels]).astype('float').reshape(-1, 2)
-        else:
-            labels = self.label_frame.iloc[idx, 1]
-            labels = (np.array(labels)).astype("int")
 
         if (self.transform):
             images = self.transform(images)
+        
+        if self.mode == 'reg':
+            labels = self.label_frame.iloc[idx, 1:]
+            labels = np.array([labels]).astype('float').reshape(-1, 2)
+            return images, labels
+        elif self.mode == 'class':
+            labels = self.label_frame.iloc[idx, 1]
+            labels = (np.array(labels)).astype("int")
+            return images, labels
+        elif self.mode == 'both':
+            labels_class = self.label_frame.iloc[idx, 1]
+            labels_class = (np.array(labels_class)).astype("int")
+            labels_reg = self.label_frame.iloc[idx, 2:]
+            labels_reg = np.array([labels_reg]).astype('float').reshape(-1, 2)
+            return images, labels_reg, labels_class
 
         return images, labels
 
 
 class Dataloader():
-
     def __init__(
         self, root, image_dir, label_frame,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.5686, 0.4505, 0.3990],
-                std=[0.2332, 0.2064, 0.1956])]),
-        regression=False, subset = None
+        transform=transforms.Compose([transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5686, 0.4505, 0.3990],std=[0.2332, 0.2064, 0.1956])]),
+        mode=None, subset = None
     ):
         """
         Args:
@@ -71,7 +76,7 @@ class Dataloader():
         self.root = root
         self.image_dir = root + image_dir
         self.label_frame = root + "data/" + label_frame
-        self.regression = regression
+        self.mode = mode
         self.transform = transform
         self.subset = subset
 
@@ -87,7 +92,7 @@ class Dataloader():
                 will return the corresponding batchX, batchY.
         """
         
-        dataset = Dataset(self.image_dir, self.label_frame, subset=self.subset, transform=self.transform, regression=self.regression)
+        dataset = Dataset(self.image_dir, self.label_frame, subset=self.subset, transform=self.transform, mode=self.mode)
         batch_iter = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
         return batch_iter
